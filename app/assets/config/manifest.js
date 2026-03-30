0b16730486155a5feecfbde6f9c8387928b86984
nao-7777 <snbgzr0609@au.com>
2026-03-31 08:26:09 +0900

    styleseet

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,14 @@
 // app/assets/config/manifest.js
 
+// 画像フォルダを丸ごと読み込む
 //= link_tree ../images
+
+// スタイルシートフォルダを読み込む（.cssファイルのみ）
 //= link_directory ../stylesheets .css
-//= link_tree ../builds .js
+
+// JS関連（もしビルド済みファイルがあるなら）
 //= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
+// Tailwindなど特定のファイル
 //= link tailwind.css
