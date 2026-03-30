541466c811a9fd43136e9bbe38a1ae109e3273a3
nao-7777 <snbgzr0609@au.com>
2026-03-31 08:43:08 +0900

    Fix manifest paths based on actual folder structure

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,13 +1,6 @@
 // app/assets/config/manifest.js
 
-// 1. 画像（asanoha.png や logomark.png がある場所）
 //= link_tree ../images
-
-// 2. CSS（あなたが application.css を置いている場所）
 //= link_directory ../tailwind .css
-
-// 3. JS（標準的な階層に絞る）
+//= link_tree ../builds .css
 //= link_tree ../../javascript .js
-
-// 4. 個別指定
-//= link tailwind.css
