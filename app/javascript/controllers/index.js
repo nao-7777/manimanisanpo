41d8d0cff33f2addc092b33a24743b730ce2dbf1
nao-7777 <snbgzr0609@au.com>
2026-03-31 07:37:34 +0900

    fox

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,6 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-// 💡 ファイル名 walking_mode_controller.js と完全に一致させる
+// ファイル名 walking_mode_controller.js と完全に一致させる
 import WalkingModeController from "./walking_mode_controller"
 application.register("walking-mode", WalkingModeController)
