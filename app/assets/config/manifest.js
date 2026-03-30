a90bd30f56b7b478ba530a04b18d8d8a50bbfa32
nao-7777 <snbgzr0609@au.com>
2026-03-31 08:36:58 +0900

    Remove non-existent vendor path from manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,15 +1,13 @@
 // app/assets/config/manifest.js
 
-// 1. 画像フォルダ
+// 1. 画像（asanoha.png や logomark.png がある場所）
 //= link_tree ../images
 
-// 2. CSSフォルダ
-// stylesheets ではなく tailwind フォルダを指定します
+// 2. CSS（あなたが application.css を置いている場所）
 //= link_directory ../tailwind .css
 
-// 3. JS関連
+// 3. JS（標準的な階層に絞る）
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 
 // 4. 個別指定
 //= link tailwind.css
