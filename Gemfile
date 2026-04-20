e2e0d9fd66f8e8d5f1a8e9abdcace8bf661c1834
nao-7777 <snbgzr0609@au.com>
2026-04-20 19:48:44 +0900

    OGP設定

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
 end
+
+gem 'meta-tags'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'まにまに散歩',
+      title: '神様と歩く、いつもの道。',
+      reverse: true,
+      separator: '|',
+      description: '目的地のない散歩は、もう終わり。あなたの何気ない歩みが、姿を忘れてしまった「土地神様」の力になります。',
+      keywords: '散歩,土地神様,育成,ウォーキング',
+      canonical: request.original_url,
+      og: {
+        site_name: 'まにまに散歩',
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('OGP.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image', # 画像を大きく見せるタイプ
+      }
+    }
+  end
 end
